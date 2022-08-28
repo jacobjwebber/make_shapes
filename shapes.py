5d392dfae27fbf2583198c9946ab87f963a20b47
jjw <jacobjwebber@gmail.com>
2022-08-28 18:30:20 +0100

    first prototype

--- shapes.py
+++ shapes.py
@@ -1,6 +1,6 @@
 import svgwrite
 from svgwrite import cm, mm
-from PIL import Image, ImageOps, ImageDraw
+from PIL import Image, ImageOps, ImageDraw, ImageFont
 
 
 shape = '''\
@@ -20,34 +20,32 @@ shape = '''\
 '''
 U = 200# * mm # base unit
 
-print(shape)
-
-def draw_shape(shape, name):
-    dwg = svgwrite.Drawing(filename=name, debug=True)
-    shapes = dwg.add(dwg.g(id='shapes'))
-    for i, line in enumerate(shape.splitlines()):
-        for j, char in enumerate(line):
-            if not char.isspace():
-                shapes.add(dwg.rect(insert=(j*U, i*U), size=(U, U)))
-
-
-    dwg.save()
-
 def pil_draw_shape(shape):
+    text = "🐟6\n⌛2"
     shape = shape.splitlines()
     X = max([len(line) for line in shape])
     Y = len(shape)
     print(X, Y)
-    out = Image.new('L', (X*U, Y*U), 0)
+    out = Image.new('1', (X*U, Y*U), 0)
     d = ImageDraw.Draw(out)
+    first = True
     for i, line in enumerate(shape):
         for j, char in enumerate(line):
             if not char.isspace():
-                x = i*U
-                y = j*U
-                d.rectangle([(x,y), (x+U,y+U)], fill=100000)
+                y = i*U
+                x = j*U
+                d.rectangle([(x,y), (x+U,y+U)], fill=1)
+                if first:
+                    text_coords = (x+0.1*U,y+ 0.1*U)
+                    first = False
                 print(x, y)
-    out.show()
+
+    img = ImageOps.fit(Image.open('coral1.jpg'),out.size)
+    img.putalpha(out)
+    d = ImageDraw.Draw(img)
+    fnt = ImageFont.truetype("./NotoEmoji-Medium.ttf", 60)
+    d.text(text_coords, text, font=fnt, fill=(0,0,0))
+    img.show()
 
 
 
